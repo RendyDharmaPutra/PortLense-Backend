ac48c20c5ce643ba4da401ca7747283f10c87a8e
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2026-05-12 15:23:39 +0700

    feat: add dotenv support to configure server port via environment variables

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@ mod api;
 mod services;
 mod core;
 
+use std::env;
+
 use axum::{routing::get, Router};
 use api::handlers::port_handlers::ports_handler;
+use dotenvy::dotenv;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let port = env::var("PORT").expect("PORT not found in .env");
+
     let app = Router::new().route("/ports", get(ports_handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
         .unwrap();
 
-    println!("Server running on http://localhost:3000");
+    println!("Server running on http://localhost:{}", port);
 
     axum::serve(listener, app).await.unwrap();
 }
